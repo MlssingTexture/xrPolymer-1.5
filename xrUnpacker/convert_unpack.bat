10993cebb60bcaa9d14f17fde450925ea5b770ba
MissingTexture <89803967+MlssingTexture@users.noreply.github.com>
2021-12-19 14:17:54 -0500

    UPD 0.7

---
+++ xrUnpacker/convert_unpack.bat
@@ -0,0 +1,14 @@
+mkdir .\unpacked
+for %%f in (.\db\*.db*) do (
+    converter.exe -unpack -xdb %%f -dir .\unpacked
+)
+for %%f in (.\db\levels\*.db*) do (
+    converter.exe -unpack -xdb %%f -dir .\unpacked
+)
+for %%f in (.\db\mods\*.db*) do (
+    converter.exe -unpack -xdb %%f -dir .\unpacked
+)
+for %%f in (.\db\updates\*.db*) do (
+    converter.exe -unpack -xdb %%f -dir .\unpacked
+)
+pause
